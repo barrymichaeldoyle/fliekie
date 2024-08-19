cf703e9cf78e8f9bc1aa6dc9669c345c5ca6c265
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2024-08-19 18:42:14 +0200

    test github actions

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -7,6 +7,8 @@ import { Button, type ButtonProps } from "./ui/button";
 export function SubmitButton(props: ButtonProps) {
   const { pending } = useFormStatus();
 
+  console.log("Testing GitHub Actions");
+
   return (
     <Button isLoading={pending || props.isLoading} type="submit" {...props} />
   );
