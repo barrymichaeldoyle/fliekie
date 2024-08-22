4c96eef899d7177ccc11b24021b8ffed54e3dcd9
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2024-08-22 10:00:55 +0200

    added simple favicon

--- src/app/_layout/TopNav.tsx
+++ src/app/_layout/TopNav.tsx
@@ -5,7 +5,7 @@ import { SearchInput } from "./SearchInput";
 export function TopNav() {
   return (
     <nav className="flex min-h-14 w-full items-center justify-between border-b bg-card px-6 text-xl font-semibold">
-      <div>Fliekie</div>
+      <div>🍿 Fliekie</div>
 
       <div className="flex items-center gap-4">
         <SearchInput />
