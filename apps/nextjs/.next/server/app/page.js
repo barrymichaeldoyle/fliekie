135eb832d47a6c8aeeb4ecce5dbf669cf6289528
Barry Michael Doyle <barry@barrymichaeldoyle.com>
2024-08-28 15:44:31 +0200

    get rid of next folder

--- apps/nextjs/.next/server/app/page.js
+++
@@ -1,9 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/page.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/8079e_next_5a9329._.js");
-runtime.loadChunk("server/chunks/ssr/[project]__8bf038._.js");
-runtime.loadChunk("server/chunks/ssr/9c2d3_@clerk_backend_dist_07f261._.js");
-runtime.loadChunk("server/chunks/ssr/08b5e__pnpm_dfcc4a._.js");
-runtime.loadChunk("server/chunks/ssr/src_3fa486._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/apps/nextjs/.next-internal/server/app/page/actions.js { ACTIONS_MODULE0 => \"[project]/node_modules/.pnpm/@clerk+nextjs@5.3.6_next@14.2.7_react-dom@18.3.1_react@18.3.1/node_modules/@clerk/nextjs/dist/esm/app-router/server-actions.js [app-rsc] (ecmascript, action, ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@14.2.7_react-dom@18.3.1_react@18.3.1/node_modules/next/dist/esm/build/templates/app-page.js?page=/page { COMPONENT_0 => \"[project]/apps/nextjs/src/app/layout.tsx [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_1 => \"[project]/apps/nextjs/src/app/loading.tsx [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_2 => \"[project]/apps/nextjs/src/app/not-found.tsx [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_3 => \"[project]/apps/nextjs/src/app/page.tsx [app-rsc] (ecmascript, Next.js server component)\" } [app-rsc] (ecmascript) <facade>", CHUNK_PUBLIC_PATH).exports;
--- apps/nextjs/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/apps/nextjs/src/app/page\": [\n      \"static/media/Geist_Variable-s.p.781b491f.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
